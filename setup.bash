19f8aa9b2fd5fa02e2aa6afdbf680b727271f18e
thewildpenguin <gaetan.puleo.dev@gmail.com>
2021-04-26 14:14:04 +0200

    fix: setup script

--- setup.bash
+++ setup.bash
@@ -1,11 +1,5 @@
 # install LSP servers
-npm -g install tsun \ 
-	vscode-html-languageserver-bin \ 
-	vscode-css-languageserver-bin \
-	typescript \
-	typescript-language-server \
-	vscode-json-languageserver \
-	diagnostic-languageserver
+npm -g install tsun	vscode-html-languageserver-bin vscode-css-languageserver-bin typescript typescript-language-server vscode-json-languageserver diagnostic-languageserver
 
 # install ESLint
-npm - g install eslint_d
+npm -g install eslint_d
